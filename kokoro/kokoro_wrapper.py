0b8b2d79c099ce235675348fd9e0d5e2104394dc
aggrv8daslt <tampafasttek@gmail.com>
2025-07-14 13:12:01 -0700

    Update kokoro_wrapper.py

--- kokoro/kokoro_wrapper.py
+++ kokoro/kokoro_wrapper.py
@@ -1,18 +1,23 @@
+# kokoro/kokoro_wrapper.py
+
 from kokoro.core import generate
 
 class KokoroTTS:
-    def __init__(self, model, lang="en", speed=1.0, voicepack=None):
+    def __init__(self, model, lang='a', speed=1.0):
         self.model = model
         self.lang = lang
         self.speed = speed
-        self.voicepack = voicepack
 
-    def synthesize(self, text, speaker_embedding=None):
-        return generate(
+    def synthesize(self, text, speaker_embedding, ps=None):
+        print(f"[DEBUG] synthesize() received ps type: {type(ps)}")
+        
+        audio, phonemes = generate(
             self.model,
             text,
-            self.voicepack,
-            self.lang,
-            self.speed,
-            speaker_embedding
+            speaker_embedding,
+            lang=self.lang,
+            speed=self.speed,
+            ps=ps
         )
+
+        return audio, phonemes
